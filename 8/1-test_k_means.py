2e4815a0754833ebdf2b677d3e9aae750229240c
Daniel Lindestad <lindestad@proton.me>
2025-03-04 13:02:18 +0100

    Improved 8 - 1

--- 8/1-test_k_means.py
+++ 8/1-test_k_means.py
@@ -6,23 +6,29 @@ import matplotlib.pyplot as plt
 def main():
     X, y = make_blobs(n_samples=300, centers=3, cluster_std=0.5, random_state=0)
 
-    centroids, labels, inertia = k_means(X, n_clusters=3, random_state=0)
-
-    print(X.shape)
-    print(y.shape)
     # Plotting
-    plt.figure(figsize=(12, 5))
+    plt.figure(figsize=(12, 9))
+
     # Plot true labels
-    plt.subplot(1, 2, 1)
+    plt.subplot(2, 2, 1)
     plt.scatter(X[:, 0], X[:, 1], c=y)
     plt.title("True Labels")
 
-    # Plot k-means predicted labels
-    plt.subplot(1, 2, 2)
-    plt.scatter(X[:, 0], X[:, 1], c=labels)
-    # Plot centroids as red X-es
-    plt.scatter(centroids[:, 0], centroids[:, 1], s=200, c="red", marker="X")
-    plt.title("K-Means Predicted Labels")
+    config = [
+        {"K": 3, "plot_idx": 2},
+        {"K": 4, "plot_idx": 3},
+        {"K": 6, "plot_idx": 4},
+    ]
+
+    for c in config:
+        centroids, labels, inertia = k_means(X, n_clusters=c["K"], random_state=0)
+
+        # Plot k-means predicted labels
+        plt.subplot(2, 2, c["plot_idx"])
+        plt.scatter(X[:, 0], X[:, 1], c=labels)
+        # Plot centroids as red X-es
+        plt.scatter(centroids[:, 0], centroids[:, 1], s=200, c="red", marker="X")
+        plt.title(f"K-Means Predicted Labels for K = {c["K"]}.")
 
     # Display the plots
     plt.show()
